be0673587f50f398d6d34ee133d86c9619cdd634
dimitris negkas <dimneg@gmail.com>
2018-02-18 13:09:39 +0200

    pre process

--- results.php
+++ results.php
@@ -15,14 +15,207 @@ class results {
     function showResults(){
         global $Actual_link;
         global $Lang;
+        global $prefix;
+        global $Results;
+        $source = ' ';
         $Actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $double=[[]];
         if (strpos($Actual_link,'/en/') !== false) {
             $Lang = 'en/';
         }
         else {
              if (strpos($Actual_link,'/el/') !== false) {
-                $Lang= 'el/';
+                $Lang = 'el/';
+             }
+             else {
+                 $Lang = '';
              }
         }
+        
+        $uniqueResults = $this->unique_multidim_array($Results,'link');
+        foreach ($uniqueResults as $key => $row) {
+            $id[$key]  = $row['id']; 	  
+        }
+        array_multisort($id, SORT_ASC, $uniqueResults );
+        $uniqueResults  = array_reverse($uniqueResults );
+        $last_item = NULL;
+        $last_item_2 = NULL;
+        $last_item_3 = NULL;
+        $last_item_4 = NULL;
+        $numItems = count($uniqueResults);
+        $c = 0;
+        foreach ($uniqueResults as $key => &$row) { //diaygeia + khmdhs (2 records) become  diaygeiakhmdhs (1 record) for buyer and one for seller if exist
+            if   (strpos($row['link'],'diaugeia/organization-hybrid') !== false){
+                $row['link'] = str_replace('diaugeia/organization-hybrid','zdiaugeia/diaugeia-hybrids',$row['link']);
+            }
+            if   (strpos($row['link'],'organization-beneficiary') !== false){
+                $row['link']=str_replace('organization-beneficiary','zorganization-beneficiary',$row['link']);
+            }
+            if ($last_item['id'] === $row['id'])   {
+                $double[] = $last_item; //copy doubles to other table
+                if (strpos($row['link'],'/diaugeia/') !== false ) { //DIAUGEIA
+                    $row['dk_flag']='d';
+	            $row['link'] =  str_replace('diaugeia','diaugeiakhmdhs', $row['link'] );			
+	            $row['lastUpdate_2'] = $last_item['lastUpdate'];			
+		    $row['contractAmountPrev_2'] = $last_item['contractAmountPrev'];
+		    $row['contractAmountCur_2'] = $last_item['contractAmountCur'];
+		    $row['paymentAmountPrev_2'] = $last_item['paymentAmountPrev'];
+		    $row['paymentAmountCur_2'] = $last_item['paymentAmountCur'];
+		    $row['contractItemsNo_2'] = $last_item['contractItemsNo'];
+		    $row['paymentItemsNo_2'] = $last_item['paymentItemsNo']; 
+			
+                }
+                else {
+                     if (strpos($row['link'],'/khmdhs/') !== false ) 	{ //KHMDHS
+                        $row['dk_flag']='k';
+			$row['link'] =  str_replace('khmdhs','diaugeiakhmdhs', $row['link'] );
+			$row['address'] =  $last_item['address'];			
+			$row['pc'] =  $last_item['pc'];
+			$row['city'] =  $last_item['city'];
+			$row['lastUpdate_2']=$last_item['lastUpdate'];			
+			$row['award0_2']=$last_item['award0'];
+			$row['award1_2']=$last_item['award1'];	
+			$row['award2_2']=$last_item['award2'];	
+			$row['awardCnt0_2']=$last_item['awardCnt0'];	
+			$row['awardCnt1_2']=$last_item['awardCnt1'];	
+			$row['awardCnt2_2']=$last_item['awardCnt2'];			  
+			$row['spend0_2']=$last_item['spend0'];
+			$row['spend1_2']=$last_item['spend1'];
+			$row['spend2_2']=$last_item['spend2'];
+			$row['spendCnt0_2']=$last_item['spendCnt0'];
+			$row['spendCnt1_2']=$last_item['spendCnt1'];
+			$row['spendCnt2_2']=$last_item['spendCnt2'];
+                    }
+                }
+                 $row['relative1'] = $last_item['score']; //boost step 3
+            }
+            if(++$c !== $numItems) {
+                $last_item = $row;
+	        $last_item_2 = $last_item;
+            }
+        }
+        $c = 0;
+        $toMerge = 0;
+        foreach ($uniqueResults as $key => &$row){
+            $sort['id'][$key] = substr($row['id'],strpos($row['id'], "?=") );
+            $sort['link'][$key] = $row['link'];
+        }
+        array_multisort($sort['id'], SORT_ASC, $sort['link'], SORT_ASC,$uniqueResults );
+        foreach ($uniqueResults as $key => &$row){
+            if (strpos($row['link'],'zdiaugeia') !== false){
+                if ((substr($row['id'],strpos($row['id'], "?=") )) == (substr($last_item['id'],strpos($last_item['id'], "?=") ))){
+                    if ($row['relative1'] == 0) {
+                        $row['relative1'] = $last_item['score']; //boost rel1
+                    }
+                    if ((strpos($last_item['link'],'/diaugeia/') !== false ) || (strpos($last_item['link'],'/diaugeiakhmdhs/') !== false ) ) {
+                        $double[] = $last_item;
+                    }
+                    if (strpos($last_item['link'],'/khmdhs/') !== false ) {
+                        $double[]=$last_item;
+                        if (strpos($last_item['link'],'seller')!== false){
+                            $row['linkHKS']=$last_item['link'];		
+                            $row['contractAmountPrevHKS']=$last_item['contractAmountPrev']; 
+                            $row['contractAmountCurHKS']=$last_item['contractAmountCur'];
+                            $row['paymentAmountPrevHKS']=$last_item['paymentAmountPrev'];
+                            $row['paymentAmountCurHKS']=$last_item['paymentAmountCur'];
+                            $row['contractItemsNoHKS']=$last_item['contractItemsNo'];
+                            $row['paymentItemsNoHKS']=$last_item['paymentItemsNo'];
+                            $row['lastUpdateHKS']=$last_item['lastUpdate'];	 
+                        }
+                        else {
+                            $row['linkHKB']=$last_item['link'];	
+                            $row['contractAmountPrevHKB']=$last_item['contractAmountPrev']; 
+                            $row['contractAmountCurHKB']=$last_item['contractAmountCur'];
+                            $row['paymentAmountPrevHKB']=$last_item['paymentAmountPrev'];
+                            $row['paymentAmountCurHKB']=$last_item['paymentAmountCur'];
+                            $row['contractItemsNoHKB']=$last_item['contractItemsNo'];
+                            $row['paymentItemsNoHKB']=$last_item['paymentItemsNo'];	 
+                            $row['lastUpdateHKB']=$last_item['lastUpdate'];	 
+                        }
+                    }
+                    if ((substr($last_item_2['id'],strpos($last_item_2['id'], "?=") )) == (substr($last_item['id'],strpos($last_item['id'], "?=") ))){
+                         $row['relative2']=$last_item_2['score']; //boost rel2
+                         if ((strpos($last_item_2['link'],'/diaugeia/') !== false ) || (strpos($last_item_2['link'],'/diaugeiakhmdhs/') !== false ) ) {
+                             $double[] = $last_item_2;                             
+                         }
+                         if (strpos($last_item_2['link'],'/khmdhs/') !== false ) {
+                             $double[]=$last_item_2;
+                             if (strpos($last_item_2['link'],'seller')!== false){
+                                 $row['linkHKS']=$last_item_2['link'];			
+                                 $row['contractAmountPrevHKS']=$last_item_2['contractAmountPrev']; 
+                                 $row['contractAmountCurHKS']=$last_item_2['contractAmountCur'];
+                                 $row['paymentAmountPrevHKS']=$last_item_2['paymentAmountPrev'];
+                                 $row['paymentAmountCurHKS']=$last_item_2['paymentAmountCur'];
+                                 $row['contractItemsNoHKS']=$last_item_2['contractItemsNo'];
+                                 $row['paymentItemsNoHKS']=$last_item_2['paymentItemsNo'];
+                                 $row['lastUpdateHKS']=$last_item_2['lastUpdate'];	
+                             }
+                             else {
+                                 $row['linkHKB']=$last_item_2['link'];
+                                 $row['contractAmountPrevHKB']=$last_item_2['contractAmountPrev']; 
+                                 $row['contractAmountCurHKB']=$last_item_2['contractAmountCur'];
+                                 $row['paymentAmountPrevHKB']=$last_item_2['paymentAmountPrev'];
+                                 $row['paymentAmountCurHKB']=$last_item_2['paymentAmountCur'];
+                                 $row['contractItemsNoHKB']=$last_item_2['contractItemsNo'];
+                                 $row['paymentItemsNoHKB']=$last_item_2['paymentItemsNo'];	 
+                                 $row['lastUpdateHKB']=$last_item_2['lastUpdate'];	
+                             }
+                         }
+                         if ((substr($last_item_3['id'],strpos($last_item_3['id'], "?=") )) == (substr($last_item_2['id'],strpos($last_item_2['id'], "?=") ))){
+                             $row['relative3'] = $last_item_3['score']; //boost rel3
+                             if ((strpos($last_item_3['link'],'/diaugeia/') !== false ) || (strpos($last_item_3['link'],'/diaugeiakhmdhs/') !== false ) ) {
+                                 $double[]=$last_item_3;
+                             }
+                             if (strpos($last_item_3['link'],'/khmdhs/') !== false ) {
+                                 $double[]=$last_item_3;
+                                 if (strpos($last_item_3['link'],'seller')!== false){
+                                     $row['linkHKS']=$last_item_3['link'];						
+				     $row['contractAmountPrevHKS']=$last_item_3['contractAmountPrev']; 
+				     $row['contractAmountCurHKS']=$last_item_3['contractAmountCur'];
+				     $row['paymentAmountPrevHKS']=$last_item_3['paymentAmountPrev'];
+				     $row['paymentAmountCurHKS']=$last_item_3['paymentAmountCur'];
+				     $row['contractItemsNoHKS']=$last_item_3['contractItemsNo'];
+				     $row['paymentItemsNoHKS']=$last_item_3['paymentItemsNo'];
+				     $row['lastUpdateHKS']=$last_item_3['lastUpdate'];	
+                                 }
+                                 else {
+                                     $row['linkHKB']=$last_item_3['link'];
+				     $row['contractAmountPrevHKB']=$last_item_3['contractAmountPrev']; 
+				     $row['contractAmountCurHKB']=$last_item_3['contractAmountCur'];
+				     $row['paymentAmountPrevHKB']=$last_item_3['paymentAmountPrev'];
+				     $row['paymentAmountCurHKB']=$last_item_3['paymentAmountCur'];
+				     $row['contractItemsNoHKB']=$last_item_3['contractItemsNo'];
+				     $row['paymentItemsNoHKB']=$last_item_3['paymentItemsNo'];	 
+				     $row['lastUpdateHKB']=$last_item_3['lastUpdate'];	
+                                 }
+                             }
+                         }
+                    }
+                }
+            }
+        }
+    }
+    
+    function unique_multidim_array($array, $key){
+        $temp_array = [[]]; //empty array
+        $i = 0;
+        $key_array = [[]]; //empty array           
+
+        foreach($array as $val){	
+
+            if 	(!in_array($val[$key],$key_array))   { 
+                $key_array[$i] = $val[$key];
+                $temp_array[$i] = $val;
+                $i++;
+            }
+                    
+            else { 
+                
+            }
+                    
+
+        }
+        return $temp_array;
+
     }
 }
