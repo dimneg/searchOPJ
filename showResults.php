0c79c29507e77d548f054b9aa7b6329df9ec060a
dimitris negkas <dimneg@gmail.com>
2018-03-20 21:18:18 +0200

    alt names

--- showResults.php
+++ showResults.php
@@ -51,8 +51,7 @@ class showResults {
                       
                    echo "<a class='nameLink' href='#' target='_blank' >$name</a> </br>";	
                    if (!empty($uniqueResults[$i]['altNames'])) {
-                       echo 'Eμφανίζεται και ως: '.$this->hide_not_avail(implode(',',$uniqueResults[$i]['altNames'])."</br>");
-                       echo 'Eμφανίζεται και ως: '.$uniqueResults[$i]['altNames']."</br>";
+                      echo 'Eμφανίζεται και ως: '.$uniqueResults[$i]['altNames']."</br>";
                    }
                    if ($uniqueResults[$i]['dataDiaugeia'] == 1){
 
