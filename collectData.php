cf380968cbf56d2fd6f31b3a6563b433782fee12
dimitris negkas <dimneg@gmail.com>
2018-03-20 20:32:06 +0200

    alt names

--- collectData.php
+++ collectData.php
@@ -447,7 +447,7 @@ class collectData {
        $ch = curl_init();
        $url = $solrPath.$solrCore."/select?indent=on&q=id:".$vat."&wt=json";
        $url = str_replace(' ','%20',$url);	
-       #echo $url.PHP_EOL;
+       echo $url.PHP_EOL;
        curl_setopt($ch, CURLOPT_URL, $url);
        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -464,6 +464,7 @@ class collectData {
        if (isset ($json['response']['alt_names']) ){
            # return implode(', ', $json['response']['alt_names']);
            echo implode(', ', $json['response']['alt_names']);
+           print_r($json['response']['alt_names']);
             return implode(', ', $json['response']['alt_names']);
        }
        #return $json;
