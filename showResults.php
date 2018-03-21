c766e418a9f28cf0458c8ab1c1ee5d6a57a9fa43
dimitris negkas <dimneg@gmail.com>
2018-03-21 12:04:44 +0200

    a s = arrange sources

--- showResults.php
+++ showResults.php
@@ -12,9 +12,9 @@
  * @author dimitris negkas
  */
 class showResults {
-    function presentResults(){
+    function presentResults(){ //test 090166291
         global $Results;
-        $this->saveCsvCloud($Results, '/var/log/results.csv');
+       # $this->saveCsvCloud($Results, '/var/log/results.csv');
         #print_r($Results);
         $source = ' ';
         $i = 0;
