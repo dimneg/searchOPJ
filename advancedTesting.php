acb5af373f98fad7a9e7f7ba78eaec89a2104b93
dimitris negkas <dimneg@gmail.com>
2018-03-25 19:35:23 +0300

    testinh

--- advancedTesting.php
+++ advancedTesting.php
@@ -16,3 +16,26 @@ What is your Gender?
   <option value="F">Female</option>
 </select>
 </p>
+
+<label for='formCountries[]'>Select the countries that you have visited:</label><br>
+<select multiple="multiple" name="formCountries[]">
+    <option value="US">United States</option>
+    <option value="UK">United Kingdom</option>
+    <option value="France">France</option>
+    <option value="Mexico">Mexico</option>
+    <option value="Russia">Russia</option>
+    <option value="Japan">Japan</option>
+</select>
+<?php
+
+if(isset($_POST['formSubmit']) )
+{
+  $varMovie = $_POST['formMovie'];
+  $varName = $_POST['formName'];
+  $varGender = $_POST['formGender'];
+  $errorMessage = "";
+
+  // - - - snip - - - 
+}
+
+#scenario keyword + country
