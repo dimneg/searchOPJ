4d5b32642e36b35c38a6e4092901954c9343f4d0
dimitris negkas <dimneg@gmail.com>
2018-03-25 20:59:53 +0300

    adv

--- index.php
+++ index.php
@@ -243,7 +243,7 @@
             }
             
             body{
-			width: 900px;
+			 <!--width: 900px;-->
 			font-family: "Segoe UI",Optima,Helvetica,Arial,sans-serif;
 			line-height: 25px;
 		}
