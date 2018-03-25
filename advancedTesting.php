94ec2485851f8f3bff5b3cb6bb998a095160c170
dimitris negkas <dimneg@gmail.com>
2018-03-25 11:41:58 +0300

    test

--- advancedTesting.php
+++ advancedTesting.php
@@ -2,12 +2,9 @@
 
 <label for='formCountries[]'>Select the countries that you have visited:</label><br>
 <select multiple="multiple" name="formCountries[]">
-    <option value="US">United States</option>
-    <option value="UK">United Kingdom</option>
-    <option value="France">France</option>
-    <option value="Mexico">Mexico</option>
-    <option value="Russia">Russia</option>
-    <option value="Japan">Japan</option>
+    <option value="GR">Ελλάδα</option>
+    <option value="UK">Ηνωμένο Βασίλειο</option>
+    
 </select>
 
 
