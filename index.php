4a7de9802acaad86432e0df648f06205ec262394
dimitris negkas <dimneg@gmail.com>
2018-03-28 17:34:15 +0300

    adv testing

--- index.php
+++ index.php
@@ -328,7 +328,7 @@
 				<div>
 					<input type="submit" name="formSubmit" class="btnSearch" value="Search" action="index.php" method="post" accept-charset="UTF-8">
                                   
-                                  
+                                       <a input type="submit" name="formSubmit" method="post" accept-charset="UTF-8" class="btnSearch" href="index.php?varKeyword=<?php if (isset($_POST['formKeyword'])) echo $_POST['formKeyword']?>"   ></a>
 				</div>
 
 </p>
