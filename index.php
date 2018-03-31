1163399393f6431ef6798256a35033186d1741f9
dimitris negkas <dimneg@gmail.com>
2018-03-31 19:03:20 +0300

    choice amount

--- index.php
+++ index.php
@@ -394,7 +394,7 @@ $crf3 = '';
 #print_r($_POST['advSearch']);
 $advChoiceArea = $_POST['advSearch']['search_in_area'];
 $advChoiceAmount = $_POST['advSearch']['search_in_amount'];
-echo $advChoiceAmount; 
+#echo $advChoiceAmount; 
 #echo 'choices:'.$advChoiceArea.' '.$advChoiceAmount.PHP_EOL;   
 
 #adv search variables
