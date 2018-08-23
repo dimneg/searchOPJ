a80664c318c07d4ae3bd74e085a41726d6a9e61c
dimitris negkas <dimneg@gmail.com>
2018-08-23 17:40:01 +0300

    new for aug 18

---
+++ corporates_couch.php
@@ -0,0 +1,140 @@
+<?php
+include     'config.php';    
+$ch = curl_init();
+$couchUserPwd = couchUser.':'.couchPass;
+$db = 'yds_corporation_companies';
+$array = [
+    ['_id' =>'or1', 'db_id'=>'TEDS_2963979','source'=>'dataTed','name' =>'Oracle Belgium BVBA', 'country' =>'BE',  'core'=> 1, 'coreName'=>'ORACLE CORPORATION'],
+    ['_id' =>'or2', 'db_id'=>'TEDS_3352852','source'=>'dataTed','name' =>'Oracle Corporation UK Limited', 'country' =>'UK', 'core'=> 1, 'coreName'=>'ORACLE CORPORATION'],
+    ['_id' =>'or3', 'db_id'=>'TEDS_2259837','source'=>'dataTed','name' =>'Oracle Danmark APS', 'core'=> 1,'country' =>'DK' ,'coreName'=>'ORACLE CORPORATION'],
+    ['_id' =>'or4', 'db_id'=>'TEDS_3019308','source'=>'dataTed','name' =>'Oracle Hungary Kft.', 'core'=> 1, 'country' =>'HU','coreName'=>'ORACLE CORPORATION'], 
+    ['_id' => 'or5', 'db_id'=>'800420948','source'=>'dataDiaugeia','name' =>'ORACLE ΕΛΛΑΣ ΕΜΠΟΡΙΑ ΛΟΓΙΣΜΙΚΟΥ ΚΑΙ ΣΥΣΤΗΜΑΤΩΝ ΠΛΗΡΟΦΟΡΙΚΗΣ ΜΟΝΟΠΡΟΣΩΠΗ ΕΤΑΙΡΕΙΑ ΠΕΡΙΟΡΙΣΜΕΝΗΣ ΕΥΘΥΝΗΣ ||ORACLE ΕΛΛΑΣ Μ Ε Π Ε ', 'country' =>'GR', 'core'=> 1, 'coreName'=>'ORACLE CORPORATION'],
+    ['_id' => 'or6', 'db_id'=>'094253457','source'=>'dataDiaugeia','name' =>'ORACLE ΕΛΛΑΣ ΑΝΩΝΥΜΗ ΕΜΠΟΡΙΚΗ ΕΤΑΙΡΕΙΑ', 'country' =>'GR', 'core'=> 1, 'coreName'=>'ORACLE CORPORATION'], 
+    ['_id' => 'or7', 'db_id'=>'16957131926','source'=>'dataAustralia','name' =>'ORACLE CORPARTION', 'core'=> 1, 'country' =>'AU',  'coreName'=>'Oracle'], 
+    ['_id' => 'or8', 'db_id'=>'80003074468','source'=>'dataAustralia','name' =>'ORACLE UNIVERSITY', 'core'=> 1,'country' =>'AU',  'coreName'=>'Oracle'], 
+    ['_id' => 'or9', 'db_id'=>'998476613','source'=>'dataGemh','name' =>'Oracle ανώνυμη εταιρεία λογισμικού χρηματοοικονομικών υπηρεσιών ', 'country' =>'GR',  'core'=> 1, 'coreName'=>'ORACLE CORPORATION'], 
+    ['_id' => 'or10', 'db_id'=>'71474078909','source'=>'dataAustralia','name' =>'ORACLE USER GROUP', 'core'=> 1, 'country' =>'AU','coreName'=>'ORACLE CORPORATION'], 
+    ['_id' => 'or11', 'db_id'=>'19063655774','source'=>'dataAustralia','name' =>'ORACLE PTY LIMITED', 'country' =>'AU', 'core'=> 1, 'coreName'=>'ORACLE CORPORATION'], 
+    ['_id' => 'or12', 'db_id'=>'86768265615','source'=>'dataAustralia','name' =>'ORACLE CORPORATION AUSTRALIA PTY LTD', 'country' =>'AU', 'core'=> 1, 'coreName'=>'ORACLE CORPORATION'],
+    
+    ['_id' => 'pr1', 'db_id'=>'TEDS_3291647','source'=>'dataTed','name' =>'Pricewaterhousecoopers', 'country' =>'UK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'] ,
+    ['_id' => 'pr2', 'db_id'=>'TEDS_2086900','source'=>'dataTed','name' =>'PriceWaterhouseCoopers', 'country' =>'UK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr3', 'db_id'=>'TEDS_3182742','source'=>'dataTed','name' =>'Pricewaterhousecoopers','country' =>'NO',  'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr4', 'db_id'=>'TEDS_2326952','source'=>'dataTed','name' =>'PricewaterhouseCoopers LLP','country' =>'UK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr5', 'db_id'=>'TEDS_3121459','source'=>'dataTed','name' =>'PricewaterhouseCoopers LLP', 'country' =>'UK','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr6', 'db_id'=>'TEDS_2708423','source'=>'dataTed','name' =>'PricewaterhouseCoopers LLP','country' =>'UK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr7', 'db_id'=>'TEDS_2851400','source'=>'dataTed','name' =>'PricewaterhouseCoopers LLP','country' =>'UK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'], 
+    ['_id' => 'pr8', 'db_id'=>'TEDS_2204463','source'=>'dataTed','name' =>'PricewaterhouseCoopers LLP', 'country' =>'UK','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr9', 'db_id'=>'TEDS_2088190','source'=>'dataTed','name' =>'PricewaterhouseCoopers Statsautoriseret Revisionsaktieselskab','country' =>'DK', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr10', 'db_id'=>'54003311617','source'=>'dataAustralia','name' =>'PRICEWATERHOUSE COOPERS SECURITIES LTD ','country' =>'AU', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr11', 'db_id'=>'094496049','source'=>'dataDiaugeia','name' =>'PRICEWATERHOUSECOOPERS BUSINESS SOLUTIONS ΑΝΩΝΥΜΗ ΕΤΑΙΡΕΙΑ ΠΑΡΟΧΗΣ ΕΠΙΧΕΙΡΗΜΑΤΙΚΩΝ ΚΑΙ ΛΟΓΙΣΤΙΚΩΝ||PRICEWATERHOUSECOOPERS BUSINESS SOLUTIONS AE', 'country' =>'GR','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr12', 'db_id'=>'997802310','source'=>'dataDiaugeia','name' =>'PRICEWATERHOUSECOOPERS ACCOUNTING ΑΝΩΝΥΜΗ ΕΤΑΙΡΕΙΑ ΠΑΡΟΧΗΣ ΛΟΓΙΣΤΙΚΩΝ ΥΠΗΡΕΣΙΩΝ||PRICEWATERHOUSECOOPERS ACCOUNTING ΑΕ', 'country' =>'GR','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr13', 'db_id'=>'094399907','source'=>'dataDiaugeia','name' =>'ΠΡΑΙΣΓΟΥΩΤΕΡΧΑΟΥΣΚΟΥΠΕΡΣ ΑΝΩΝΥΜΗ ΕΛΕΚΤΙΚΗ ΕΤΑΙΡΕΙΑ ||PRICEWATERHOUSECOOPERS AE','country' =>'GR', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr14', 'db_id'=>'998428720','source'=>'dataDiaugeia','name' =>'ΚΟΙΝΟΠΡΑΞΙΑ PRICEWATERHOUSECOOPERS BUSINESS SOLUTIONS AE KAI PRICEWATERHOUSECOOPERS BUSINESS ADVISORS AE','country' =>'GR', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr15', 'db_id'=>'86104681290','source'=>'dataAustralia','name' =>'PRICEWATERHOUSECOOPERS PROFESSIONAL SERVICES', 'country' =>'AU','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr16', 'db_id'=>'20607773295','source'=>'dataAustralia','name' =>'PRICEWATERHOUSECOOPERS CONSULTING AUSTRALIA PTY LIMITED', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr17', 'db_id'=>'29003562696','source'=>'dataAustralia','name' =>'PRICEWATERHOUSECOOPERS ACTUARIAL PTY LIMITED','country' =>'AU', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr18', 'db_id'=>'52864604756','source'=>'dataAustralia','name' =>'PRICEWATERHOUSECOOPERS', 'country' =>'AU','core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    ['_id' => 'pr19', 'db_id'=>'57097040009','source'=>'dataAustralia','name' =>'PRICEWATERHOUSECOOPERS','country' =>'AU', 'core'=> 2, 'coreName'=>'PRICEWATERHOUSECOOPERS CORPORATION'],
+    # φερνει τα δεκα πρωτα μονο
+    ['_id' => 'nv1', 'db_id'=>'TEDS_3699892','source'=>'dataTed','name' =>'NOVARTIS FARMACEUTICA,S.A.','country' =>'ES', 'core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv2', 'db_id'=>'TEDS_3711765','source'=>'dataTed','name' =>'Novartis Finland Oy', 'core'=> 3, 'country' =>'FI','coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv3', 'db_id'=>'TEDS_3469033','source'=>'dataTed','name' =>'Novartis Healthcare A/S', 'country' =>'DK', 'core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv4', 'db_id'=>'TEDS_2190042','source'=>'dataTed','name' =>'Novartis Healthcare A/S', 'country' =>'DK','core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv5', 'db_id'=>'TEDS_4030048','source'=>'dataTed','name' =>'Novartis Healthcare A/S','country' =>'DK', 'core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv6', 'db_id'=>'TEDS_2180217','source'=>'dataTed','name' =>'Novartis Healthcare A/S Pharma', 'country' =>'DK','core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv7', 'db_id'=>'TEDS_2336288','source'=>'dataTed','name' =>'Novartis Norge AS', 'core'=> 3, 'country' =>'NO', 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv8', 'db_id'=>'TEDS_2336289','source'=>'dataTed','name' =>'Novartis Norge OTC', 'core'=> 3,'country' =>'NO', 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv9', 'db_id'=>'TEDS_2419020','source'=>'dataTed','name' =>'Novartis Sverige', 'core'=> 3,'country' =>'SE', 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv10', 'db_id'=>'094021290','source'=>'dataDiaugeia','name' =>'NOVARTIS HELLAS AEBE', 'country' =>'GR','core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv11', 'db_id'=>'60089509544','source'=>'dataAustralia','name' =>'NOVARTIS VACCINES AND', 'country' =>'AU','core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    ['_id' => 'nv12', 'db_id'=>'18004244160','source'=>'dataAustralia','name' =>'NOVARTIS PHARMACEUTICALS AUSTRALIA','country' =>'AU', 'core'=> 3, 'coreName'=>'NOVARTIS CORPORATION'],
+    # φερνει τα δεκα πρωτα μονο
+    ['_id' => 'at1', 'db_id'=>'TEDS_3971587','source'=>'dataTed','name' =>'Atos', 'country' =>'UK','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at2', 'db_id'=>'TEDS_3319151','source'=>'dataTed','name' =>'Atos Belgium SA', 'country' =>'BE','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at3', 'db_id'=>'TEDS_3291645','source'=>'dataTed','name' =>'Atos Consulting Limited','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at4', 'db_id'=>'TEDS_3462616','source'=>'dataTed','name' =>'Atos Consulting Ltd','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at5', 'db_id'=>'TEDS_2846372','source'=>'dataTed','name' =>'ATOS Consulting Ltd','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at6', 'db_id'=>'TEDS_3226509','source'=>'dataTed','name' =>'Atos Integration', 'country' =>'FR', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at7', 'db_id'=>'TEDS_3413429','source'=>'dataTed','name' =>'ATOS integration', 'country' =>'FR',  'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at8', 'db_id'=>'TEDS_3488979','source'=>'dataTed','name' =>'Atos IT Services UK Limited',  'country' =>'UK',  'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at9', 'db_id'=>'TEDS_3832868','source'=>'dataTed','name' =>'ATOS IT Services UK Limited', 'country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],   
+    ['_id' => 'at11', 'db_id'=>'TEDS_3309258','source'=>'dataTed','name' =>'Atos IT Services UK Ltd', 'country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at12', 'db_id'=>'TEDS_3289138','source'=>'dataTed','name' =>'Atos IT Solutions and Services GmbH', 'country' =>'AT','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at13', 'db_id'=>'TEDS_3211595','source'=>'dataTed','name' =>'Atos IT Solutions and Services SA', 'country' =>'LU','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at14', 'db_id'=>'TEDS_2465788','source'=>'dataTed','name' =>'Atos Origin','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at15', 'db_id'=>'TEDS_3137195','source'=>'dataTed','name' =>'Atos Origin IT Services Limited','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at16', 'db_id'=>'TEDS_2247344','source'=>'dataTed','name' =>'Atos Origin IT Services UK Limited','country' =>'UK', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at17', 'db_id'=>'TEDS_2351033','source'=>'dataTed','name' =>'Atos Origin IT Services UK Ltd', 'country' =>'UK','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at18', 'db_id'=>'TEDS_2180247','source'=>'dataTed','name' =>'Atos Worldline', 'country' =>'FR','core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at19', 'db_id'=>'094035050','source'=>'dataGemh','name' =>'ATOS IT SERVICES YΠΗΡΕΣΙΕΣ ΠΛΗΡΟΦΟΡΙΚΗΣ ΚΑΙ ΤΗΛΕΠΙΚΟΙΝΩΝΙΩΝ ΑΝΩΝΥΜΗ ΕΤΑΙΡΕΙΑ ','country' =>'GR', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at20', 'db_id'=>'800336879','source'=>'dataGemh','name' =>'(ΥΠΟ ΕΚΚΑΘΑΡΙΣΗ)ATOS Accounting and Tax Outsourcing Ανώνυμη Εταιρία Συμβουλευτικών Υπηρεσιών ','country' =>'GR', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    ['_id' => 'at21', 'db_id'=>'83401687977','source'=>'dataAustralia','name' =>'ATOS AUSTRALIA PTY LTD','country' =>'AU', 'core'=> 4,'coreName'=>'ATOS CORPORATION'],
+    
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+   
+   
+];
+#indexSolr($array);
+foreach ($array as $key => $value) {
+    $id = $value['_id'];
+    echo   $id.PHP_EOL;
+    $file_contents=json_encode($value,JSON_UNESCAPED_UNICODE);
+     curl_setopt($ch, CURLOPT_URL, 'http://83.212.86.158:5984/'.$db.'/'.$id);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); /* or PUT */
+         curl_setopt($ch, CURLOPT_POSTFIELDS, $file_contents);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_USERPWD, $couchUserPwd );
+         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-type: application/json',
+                        'Accept: */*'
+         ));
+         $response = curl_exec($ch); 
+}
+curl_close($ch);
+function indexSolr($array){
+    $ch = curl_init("http://83.212.86.164:8983/solr/corporates/update?wt=json");
+    foreach ($array as $key => $value) {
+          $data = array(
+               "add" => array( 
+                    "doc" => array(
+                         "id"   =>$value['_id'],
+                         "db_id"   =>$value['db_id'],
+                         "source"   =>$value['source'],
+                         "name"=>$value['name'],
+                         "country"=>$value['country'],
+                         "core"=>$value['core'],
+                         "coreName"=>$value['coreName']
+                         
+                        ),
+                   "commitWithin" => 1000,
+                    ),
+               );
+            $data_string = json_encode($data);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_POST, TRUE);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+
+    $response = curl_exec($ch);
+    print_r($response); 
+    }
+    
+    
+    curl_close($ch);
+    
+}
--- indexMatchingResults_couch.php
+++ indexMatchingResults_couch.php
@@ -3,6 +3,7 @@
 include 'config.php';
 include 'entity.php';
 $time_pre = microtime(true);
+$db = 'opj_alt_names';
 
 $cnt = 0;
 
@@ -15,8 +16,9 @@ if ($connLib->connect_error) {
 $libraries=['espa_sellers','ted_sellers','fek_companies','hellastat']; 
 $resultArray =[[]];
 
+echo 'source 1 library'.PHP_EOL;
 foreach ($libraries as $libTable){
-    $sql = "SELECT * FROM $libTable ".PHP_EOL;
+    $sql = "SELECT * FROM $libTable ";
     #echo  $sql;
     $result = $connLib->query($sql);
     if ($result->num_rows > 0) {
@@ -46,6 +48,39 @@ foreach ($libraries as $libTable){
         }
     }
 }
+$connLib->close();
+echo count($resultArray).PHP_EOL;
+//source 2 member position
+echo 'source 2 member position'.PHP_EOL;
+$connGemh =  new MySQLi(gemhDb_host, gemhDb_user, gemhDb_pass, gemhDb_name);
+mysqli_set_charset($connGemh,"utf8");
+$sql = "select p.vatNumber,p.name as pname, mp.name as mpname from PersonalData p  join MemberPosition  mp on p.id = mp.personID where replace (p.name,' ','') <> replace (mp.name,' ','') and  p.isGsisCompany = 1 group by p.name,mp.name ";
+$result = $connGemh->query($sql);
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+         if (isset ($row['vat']) && $row['vat'] !==''){
+             $key = searchForId($row['vat'], $resultArray,'vat');
+              if ($key === NULL){
+                   $company = new entity();
+                   $company->set_name($row['pname']);
+                   $company->set_vat($row['vatNumber']);
+                   $resultArray[$cnt]['name'] = $company->name;
+                   $resultArray[$cnt]['vat'] = $company->vat;
+                   $resultArray[$cnt]['alternate_names'] = [];
+              }
+              else {
+                   if (!in_array($row['pname'], $resultArray[$key]['alternate_names'])){ //for distinct names orelse remove
+                       $resultArray[$key]['alternate_names'][] = $row['name'];
+                   }
+              }
+         }
+         $cnt ++;
+    }
+}
+echo count($resultArray).PHP_EOL;
+$connGemh->close();
+//
+//
 #echo $cnt.PHP_EOL;
 #print_r(calculateAppearances($resultArray));
 #print_r(calculateDistribution(calculateAppearances($resultArray)));
@@ -54,8 +89,13 @@ foreach ($libraries as $libTable){
 #saveCsvCloud($resultArray, 'entities.csv');
 #saveCsvCloud(calculateAppearances($resultArray), 'appearences.csv');
 #saveCsvCloud(calculateDistribution(calculateAppearances($resultArray), 'distribution.csv'));
-print_r(calculateDistribution(calculateAppearances($resultArray)));
+#print_r(calculateDistribution(calculateAppearances($resultArray)));
 #indexSolr($resultArray);
+$couchUserPwd = couchUser.':'.couchPass;
+$ch = curl_init();
+
+indexCouch($resultArray,$ch,$couchUserPwd,$db);
+
 $time_post = microtime(true);
 $exec_time = $time_post - $time_pre;
 echo '(In '.number_format($exec_time/60,2).' mins)'.PHP_EOL ;
@@ -105,9 +145,25 @@ function calculateDistribution($array){
     }
     return $distributionArray;
 }
-function indexCouch($resultArray){
+function indexCouch($resultArray,$ch,$couchUserPwd,$db){
+    
      foreach ($resultArray as $key => $value) {
-        $file_contents=json_encode($value,JSON_UNESCAPED_UNICODE); 
+        if (!empty($value['alternate_names'])){
+             $id = $value['vat'];
+             echo   $id.PHP_EOL;
+             $file_contents = json_encode($value,JSON_UNESCAPED_UNICODE); 
+             curl_setopt($ch, CURLOPT_URL, 'http://83.212.86.158:5984/'.$db.'/'.$id);
+             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); /* or PUT */
+             curl_setopt($ch, CURLOPT_POSTFIELDS, $file_contents);
+             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+             curl_setopt($ch, CURLOPT_USERPWD, $couchUserPwd );
+             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-type: application/json',
+                        'Accept: */*'
+         ));
+         $response = curl_exec($ch); 
+        }
+       
      }
 }
 
